acde4457ac9960993262555223a6d747ba88ce53
Airon Saez <136767835+AironSaez@users.noreply.github.com>
2024-11-27 20:49:52 -0300

    creacion de plataforma Android y IOS junto con boton escanear QR

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,18 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit{
+export class HomePage implements OnInit {
 
   usuario = localStorage.getItem('loggedUser');
-
-  constructor(private router:Router) {}
-
+  scannedData: string = ''; // Para almacenar el contenido escaneado
   loggedUserName: string | null = null;
+
+  constructor(private router: Router) {}
 
   navigateToAbout() {
     this.router.navigate(['/login']);
@@ -20,13 +21,12 @@ export class HomePage implements OnInit{
 
   ngOnInit() {
     this.getLoggedUserName();
-    
   }
 
   getLoggedUserName() {
     // Recupera el nombre del usuario desde localStorage
     const storedUserName = localStorage.getItem('loggedUser');
-    console.log('Nombre de usuario recuperado: ', storedUserName);  // Agregamos este log para depurar
+    console.log('Nombre de usuario recuperado: ', storedUserName); // Agregamos este log para depurar
     this.loggedUserName = storedUserName;
   }
 
@@ -36,6 +36,29 @@ export class HomePage implements OnInit{
     this.router.navigate(['/login']);
     console.log('La sesión se ha cerrado correctamente');
   }
-  
 
+  // Nueva funcionalidad: Iniciar el escaneo QR
+  async startScanner() {
+    try {
+      const status = await BarcodeScanner.checkPermission({ force: true });
+
+      if (status.granted) {
+        BarcodeScanner.hideBackground(); // Oculta la vista web para escanear
+
+        const result = await BarcodeScanner.startScan(); // Inicia el escaneo
+        if (result.hasContent) {
+          this.scannedData = result.content; // Captura el contenido escaneado
+          console.log('Contenido escaneado:', this.scannedData);
+        } else {
+          console.log('No se encontró contenido en el QR.');
+        }
+      } else {
+        console.error('Permiso denegado para usar la cámara.');
+      }
+    } catch (error) {
+      console.error('Error al escanear el código QR:', error);
+    } finally {
+      BarcodeScanner.showBackground(); // Restaura la vista web
+    }
+  }
 }
